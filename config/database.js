a777f02de4454ea5c841ac487148a0f332d7fae0
tahafatimaDO <82136225+tahafatimaDO@users.noreply.github.com>
2021-09-25 06:12:52 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,25 @@
-module.exports = {
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  //database: env('DATABASE_NAME', 'strapi'),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
-}
+
+  //extra: {
+    //ssl: {
+     // rejectUnauthorized: false
+   // }
+ 
   
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', ';'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
